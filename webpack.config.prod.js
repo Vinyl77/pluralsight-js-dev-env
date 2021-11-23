d43466788a290f7938f3dbcaa0703668faaefb06
Richard Yvarra <77420540+Vinyl77@users.noreply.github.com>
2021-11-23 13:40:14 -0500

    changes to html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,14 +15,18 @@ export default {
     filename: "[name].[chunkhash].js",
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+    }),
     new HtmlWebpackPlugin ({
      template: "src/index.html",
+     trackJSToken:"e280de7964c8436e8267d42807862705",
     }),
   ],
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.css$/, use: [MiniCssExtractPlugin, "css-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
     ],
   },
 };
